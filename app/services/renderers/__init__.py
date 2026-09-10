d3cb50d28c6acb0f3b4a0f92b6d47880c3893cdb
Mtzallqmy <mtzallqmy@gmail.com>
2026-09-10 23:11:55 +0300

    studio: expose FFmpeg renderer

---
+++ app/services/renderers/__init__.py
@@ -0,0 +1,4 @@
+from app.services.renderers.base import BaseRenderer, RenderResult
+from app.services.renderers.ffmpeg_renderer import FFmpegRenderer, ffmpeg_renderer
+
+__all__ = ["BaseRenderer", "FFmpegRenderer", "RenderResult", "ffmpeg_renderer"]
